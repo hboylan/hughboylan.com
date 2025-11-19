b33e4a806cac0ca43185b3bd641ce15e3883acf5
Hugh Boylan <hugh@Hughs-MacBook-Pro.local>
2025-11-19 06:16:32 -0600

    fix: lint issues for img

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import Image from 'next/image'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 
@@ -14,18 +15,24 @@ export default function Layout({ children }) {
         <header className="flex flex-col items-center">
           {isHome ? (
             <>
-              <img
+              <Image
                 src="/images/profile.jpg"
+                width={128}
+                height={128}
                 className="w-32 h-32 rounded-full"
                 alt={name}
               />
-              <h1 className="text-5xl leading-tight font-extrabold tracking-tight my-4">{name}</h1>
+              <h1 className="text-5xl leading-tight font-extrabold tracking-tight my-4">
+                {name}
+              </h1>
             </>
           ) : (
             <>
               <Link href="/">
-                <img
+                <Image
                   src="/images/profile.jpg"
+                  width={96}
+                  height={96}
                   className="w-24 h-24 rounded-full"
                   alt={name}
                 />
--- components/projectCard.tsx
+++ components/projectCard.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import Link from 'next/link'
 import type React from 'react'
 import { toKebabCase } from '../utils/case'
@@ -16,15 +17,20 @@ export const ProjectCard: React.FC<ProjectCardProps> = ({
   imageUrl,
 }) => (
   <li className="bg-white rounded-xl overflow-hidden transition-all duration-300 shadow-md border border-black/5 hover:shadow-2xl hover:-translate-y-1">
-    <Link href={`/projects/${toKebabCase(name)}`} className="flex flex-col h-full">
-      <img alt={name} src={imageUrl} className="bg-contain w-full" />
+    <Link
+      href={`/projects/${toKebabCase(name)}`}
+      className="flex flex-col h-full"
+    >
+      <Image
+        alt={name}
+        src={imageUrl}
+        width={600}
+        height={400}
+        className="bg-contain w-full"
+      />
       <div className="p-4">
-        <h4 className="text-2xl leading-snug my-2">
-          {name}
-        </h4>
-        <h5 className="text-xl leading-normal text-[#bababa] m-0">
-          {company}
-        </h5>
+        <h4 className="text-2xl leading-snug my-2">{name}</h4>
+        <h5 className="text-xl leading-normal text-[#bababa] m-0">{company}</h5>
         <p>{description}</p>
       </div>
     </Link>
